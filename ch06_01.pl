df66dd620e5d15add4f98a4b5698452c032d4d1e
hajime nakamura <h.nakamura0903@gmail.com>
2021-10-03 16:14:04 +0900

    6章 練習問題1

---
+++ ch06_01.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+use List::Util qw/sum/;
+use Storable qw/retrieve store/;
+
+my $data_file = "total_bytes.dat";
+my %total_bytes;
+if (-e $data_file) {
+    %total_bytes = %{retrieve($data_file)};
+}
+
+open my $fh, "connect.dat" or die "Could not open connect.dat: $!";
+while (<$fh>) {
+    chomp;
+    my ($source, $destination, $bytes) = split;
+    $total_bytes{$source}{$destination} += $bytes;
+}
+close $fh;
+
+store \%total_bytes, $data_file;
+
+for my $source (sort keys %total_bytes) {
+    print "=== $source ===\n";
+    printf "TOTAL = %d\n", sum values %{$total_bytes{$source}};
+    
+    my @destinations = sort {$total_bytes{$source}{$b} <=> $total_bytes{$source}{$a}} keys %{$total_bytes{$source}};
+    foreach my $destination (@destinations) {
+        printf "%s => %s: %d\n", $source, $destination, $total_bytes{$source}{$destination};
+    }
+}
