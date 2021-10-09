cf8a7fea451558bff626f609be0e0cc103b19363
hajime nakamura <h.nakamura0903@gmail.com>
2021-10-09 17:46:07 +0900

    11章 練習問題1

---
+++ ch11/Oogaboogoo/Date.pm
@@ -0,0 +1,23 @@
+package Oogaboogoo::Date;
+
+use utf8;
+use strict;
+use warnings;
+
+sub number_to_day_name {
+    my $num = shift;
+    die "$num is not a valid day number" unless 0 <= $num && $num <= 6;
+
+    my @day = qw/ark dip wap sen pop sep kir/;
+    $day[$num];
+}
+
+sub number_to_month_name {
+    my $num = shift;
+    die "$num is not a valid month number" unless 0 <= $num && $num <= 11;
+
+    my @month = qw/diz pod bod rod sip wax lin sen kun fiz nap dep/;
+    $month[$num];
+}
+
+1;
